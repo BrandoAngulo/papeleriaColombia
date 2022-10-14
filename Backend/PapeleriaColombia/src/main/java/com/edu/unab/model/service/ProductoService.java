44602b1fd6e34a3dc73dfc1277911950fa342e77
Jhon Palacios <jhon52663@gmail.com>
2022-10-14 11:05:47 -0500

    agregando Repository y Service

--- Backend/PapeleriaColombia/src/main/java/com/edu/unab/model/service/ProductoService.java
+++ Backend/PapeleriaColombia/src/main/java/com/edu/unab/model/service/ProductoService.java
@@ -1,5 +1,14 @@
 package com.edu.unab.model.service;
 
+import java.util.Optional;
+
+import com.edu.unab.model.entity.Producto;
+
 public interface ProductoService {
+public Iterable<Producto>findAll();
+public Optional<Producto>findByid(Integer id);
+public Producto save(Producto p);
+public void deleteById(Integer id);
+	
 
 }
--- Backend/PapeleriaColombia/src/main/java/com/edu/unab/model/service/ProductoServiceImpl.java
+++ Backend/PapeleriaColombia/src/main/java/com/edu/unab/model/service/ProductoServiceImpl.java
@@ -1,5 +1,28 @@
 package com.edu.unab.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.edu.unab.model.entity.Producto;
+import com.edu.unab.repository.ProductoRepository;
+
+@Service
 public class ProductoServiceImpl {
+	
+	@Autowired
+	private ProductoRepository productoRepository;
+	
+	@Override
+	@Transactional(readOnly = true)
+	public Iterable<Producto> findAll(){
+		return productoRepository.findAll();
+	}
+	
+	@Override
+	@Transactional(readOnly = true)
+	public Iterable<Producto> findById(Integer id){
+		return productoRepository.findById(id);
+	}
 
 }
