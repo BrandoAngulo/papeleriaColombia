8839c28ba006ae270b7d1c55c81f69f22aa77832
BrandoAngulo <steven.sm.1992@gmail.com>
2022-10-21 02:47:14 -0500

    Se adiciono carpeta Front, con cambios realizados en, componentes,views,routes, index.html de public y se agrego el frmwork bootstrap

---
+++ frontend/papeleria_colombia/src/views/UsuarioView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Formulario/>
+    </div>
+</template>
+
+<script>
+import Formulario from '@/components/Formulario.vue';
+export default {
+    name: "Usuario-view",
+    components: {
+        Formulario
+    }
+};
+</script>
