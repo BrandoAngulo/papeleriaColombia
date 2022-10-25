da1d4ae2207eceafbcf22e9b837d3783b2efa8d0
BrandoAngulo <steven.sm.1992@gmail.com>
2022-10-24 21:03:56 -0500

    carpeta de front con modulo de usuario y sus componentes falta arreglar el formulario

---
+++ frontend/papeleria_colombia/src/views/UsuarioView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <ListarUser  :usuarios="usuarios"/>
+        <CreateUser  />
+    </div>
+</template>
+
+<script>
+import ListarUser from "@/components/usuario/ListarUser.vue";
+import CreateUser from "@/components/usuario/CreateUser.vue";
+import axios from "axios";
+
+export default {
+    name: "UsuarioView",
+    components: {
+        ListarUser, CreateUser,
+    },
+    data: () => ({
+        usuarios: [],
+    }),
+    mounted() {
+        this.getUsuarios();
+    },
+    methods: {
+       /*  refresh() {
+            this.getUsuarios();
+        }, */
+        async getUsuarios() {
+           /* const usuario = await fetch('http://localhost:3000/api/usuario/listar');
+            const res = await usuario.json();
+            console.log(res); */
+           axios
+                .get('http://localhost:3000/api/usuario/listar')
+                .then((res) => {
+                    this.usuarios = res.data;
+                    console.log(this.usuarios);
+                }); 
+        }
+
+    },
+}
+</script>
