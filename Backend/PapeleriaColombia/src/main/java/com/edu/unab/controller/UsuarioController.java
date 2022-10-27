3a9f4f521d5378b9b33fb246f3b0df4b37cf28f4
BrandoAngulo <steven.sm.1992@gmail.com>
2022-10-26 22:45:14 -0500

    modificado el back de controller usuario se implemento permisos CORS

--- Backend/PapeleriaColombia/src/main/java/com/edu/unab/controller/UsuarioController.java
+++ Backend/PapeleriaColombia/src/main/java/com/edu/unab/controller/UsuarioController.java
@@ -2,7 +2,7 @@ package com.edu.unab.controller;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.edu.unab.model.entity.Usuario;
@@ -17,7 +18,7 @@ import com.edu.unab.model.service.UsuarioService;
 
 @RestControllerAdvice
 @RequestMapping("/api/usuario")
-@CrossOrigin(origins="http://localhost:8080/")
+@CrossOrigin(origins="http://localhost:8080/", methods = {RequestMethod.POST, RequestMethod.DELETE,RequestMethod.GET})
 public class UsuarioController {
 
 	@Autowired
