0498908a16150e1ba43ffd22bb9dfb55c4c80d1e
MXCXXSs <nicolas.macias1107@gmail.com>
2022-10-30 13:31:15 -0500

    adicion del cross a ventas en el backend

--- Backend/PapeleriaColombia/src/main/java/com/edu/unab/controller/VentasController.java
+++ Backend/PapeleriaColombia/src/main/java/com/edu/unab/controller/VentasController.java
@@ -3,12 +3,14 @@ package com.edu.unab.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
@@ -17,7 +19,7 @@ import com.edu.unab.model.service.VentasService;
 
 @RestControllerAdvice
 @RequestMapping("/api/ventas")
-
+@CrossOrigin(origins="http://localhost:8080/", methods = {RequestMethod.POST, RequestMethod.DELETE,RequestMethod.GET})
 public class VentasController {
 	
 	
